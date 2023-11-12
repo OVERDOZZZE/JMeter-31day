ee0275593d15e07ef62d1249e90c7f73c7da9949
OVERDOZZZE <saparbekov113@gmail.com>
2023-11-12 21:06:39 +0600

    commit-2

--- hub.cpp
+++ hub.cpp
@@ -1,13 +1,43 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+
+        int maxStop = -1;
+        for (const auto& route : routes) {
+            for (int stop : route) {
+                maxStop = max(maxStop, stop);
+            }
+        }
+
+        if (maxStop < target) {
+            return -1;
+        }
+
+        int n = routes.size();
+        vector<int> minBusesToReach(maxStop + 1, INT_MAX);
+        minBusesToReach[source] = 0;
+
+        bool flag = true;
+        while (flag) {
+            flag = false;
+            for (const auto& route : routes) {
+                int min = n+1;
+                for (int stop : route) {
+                    min = std::min(min, minBusesToReach[stop]);
+                }
+                min++;
+                for (int stop : route) {
+                    if (minBusesToReach[stop] > min) {
+                        minBusesToReach[stop] = min;
+                        flag = true;
+                    }
                 }
             }
         }
-        return {};
+
+        return (minBusesToReach[target] < n+1) ? minBusesToReach[target] : -1;
     }
 };
